438884fe24d09d243153bf0e3060fe24284ca75f
Amirhossein Alizadeh <amirh.alzh@gmail.com>
2026-07-04 10:00:35 +0330

    First commit of dta770

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
                     <header className="bg-white shadow-md p-4 border-b border-gray-200">
                         <div className="container mx-auto flex justify-between items-center">
                             <h1 className="text-2xl font-bold text-indigo-600">
-                                Go Users Dashboard 🚀
+                                خوش آمدید
                             </h1>
                         </div>
                     </header>
